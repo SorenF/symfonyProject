4ec46f1ee9ac7b840b3553fef1c9f4dead448ede
Szymon <soren@firlej.org>
2011-07-14 20:42:27 +0200

    First commit - framework

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'E://Projects//wamp//bin//symfony//lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
